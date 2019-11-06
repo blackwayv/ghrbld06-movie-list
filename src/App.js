9c408c5cf38da3704e08a21175b83a549aba511a
Joshua Smith <lopper99@hotmail.com>
2019-11-06 14:50:20 -0700

    (init)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h2>Hello world!</h2>
+  }
+}
+
+export default App;
